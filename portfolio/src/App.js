87d5c984c73b27fc4fcfcb0a894f9abe610956b1
Oscar Plumridge <Bigop76.op@gmail.com>
2022-02-12 09:31:09 -0800

    setting up my js pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
+import React, {useState} from 'react'
 import Header from './components/Header'
 import Profile from './components/Profile'
 import Projects from './components/Projects'
 
 function App() {
   // state - which page we are on!
+  const [pageState, setPageState] = useState('Profile')
+
+  const whichPage = () => {
+    if(pageState === 'Projects') {
+      return (<Projects />)
+    } else if(pageState === 'Profile') {
+      return (<Profile />)
+    }
+  }
+
   return (
     <div className="App">
       <Header />
       <h1>Blank canvas</h1> 
+     {whichPage()}
       {/* if else stuff to show different files depening on what page the state is */}
     </div>
   );
--- portfolio/src/components/Profile.js
+++ portfolio/src/components/Profile.js
@@ -6,18 +6,18 @@ function Profile() {
             <div class="col s4 offset-s4">
                 <div class="card">
                     <div class="card-image">
-                        <img src="./assets/newOpProfilePic.jpg" id="profile_image">
-                            <span class="card-title green-text text-lighten-3">Full Stack Bootcamp Student</span>
+                        <img src="./assets/newOpProfilePic.jpg" id="profile_image"></img>
+                        <span class="card-title green-text text-lighten-3">Full Stack Bootcamp Student</span>
                     </div>
                     <div class="card-content">
-                        <p class="text-align center">I am a full stack web development student. <br> Below you will find some of the projects I have created. <br>
+                        <p class="text-align center">I am a full stack web development student. Below you will find some of the projects I have created.
                             The first two cards contain solo projects, and the third, the 1st of many team projects....</p>
-                        </div>
                     </div>
                 </div>
             </div>
-            );
-            
-        } 
+        </div>
+    );
+
+}
 
 export default Profile;
